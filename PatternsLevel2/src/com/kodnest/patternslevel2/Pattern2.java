d0f59ec20fe762b28e51bbd2b05f214d36625b7e
Roshan-h <hrithic1020@gmail.com>
2023-08-30 16:56:31 +0530

    PATTERNSLEVEL2

---
+++ PatternsLevel2/src/com/kodnest/patternslevel2/Pattern2.java
@@ -0,0 +1,16 @@
+package com.kodnest.patternslevel2;
+
+public class Pattern2 {
+ public static void main(String[] args) {
+	int i=1;
+	while(i<=5) {
+		int j=1;
+		while(j<=i) {
+			System.out.print(i+" ");
+			j++;
+		}
+		System.out.println();
+		i++;
+	}
+}
+}
